630689c1c4317c5e52c637d82d4d05e3b0b268c1
Khushi Malviya <khushimalviya11054@gmail.com>
2025-08-08 21:35:23 +0530

    Add root README

---
+++ client/src/App.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import Navbar from './components/Navbar'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './pages/home'
+import CarDetails from './pages/CarDetails'
+import Cars from './pages/Cars'
+import MyBookings from './pages/MyBookings'
+import Footer from './components/Footer'
+
+const App = () => {
+  const [showLogin, setShowLogin] = useState(false)
+  const isOwnerPath=useLocation().pathname.startsWith('/owner')
+  return (
+    <>
+      {!isOwnerPath && <Navbar setShowLogin={setShowLogin}/>}
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/car-details/:id' element={<CarDetails/>}/>
+      <Route path='/cars' element={<Cars/>}/>
+      <Route path='/my-bookings' element={<MyBookings/>}/>
+    </Routes>
+    {!isOwnerPath && <Footer />}
+    
+    </>
+  )
+}
+
+export default App
