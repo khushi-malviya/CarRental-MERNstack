ed8baa5a55881bd5a7abb7bfd2f2bdc5b7114423
Khushi Malviya <khushimalviya11054@gmail.com>
2025-08-12 00:09:01 +0530

    API's added to list owner cars, to toggle car availability, to delete a car and to get dashboard data!

--- server/controllers/ownerController.js
+++ server/controllers/ownerController.js
@@ -51,3 +51,73 @@ export const addCar = async (req,res)=>{
         res.json({success:false, message:error.message})
     }
 }
+
+//API To List Owner Cars
+export const getOwnerCars= async(req,res)=>{
+    try{
+        const {_id}=req.user;
+        const cars = await Car.find({owner:_id})
+        res.json({success:true, cars})
+    }catch (error){
+        console.log(error.message);
+        res.json({success:false, message:error.message})
+    }
+}
+
+//API to Toggle Car Availability
+export const toggleCarAvailability= async (req,res)=>{
+    try{
+        const {_id}=req.user;
+        const {carId}= req.body
+        const car = await Car.findById(carId)
+
+        //Checking is car belongs to the user
+        if(car.owner.toString() !== _id.toString()){
+            return res.json({success:false, message:"Unauthorized"});
+        }
+
+        car.isAvaliable =  !car.isAvaliable;
+        await car.save()
+        res.json({success:true, message:"Availabilty Toggled"})
+    }catch (error){
+        console.log(error.message);
+        res.json({success:false, message:error.message})
+    }
+}
+
+//API to delete a car
+export const deleteCar= async (req,res)=>{
+    try{
+        const {_id}=req.user;
+        const {carId}= req.body
+        const car = await Car.findById(carId)
+
+        //Checking is car belongs to the user
+        if(car.owner.toString() !== _id.toString()){
+            return res.json({success:false, message:"Unauthorized"});
+        }
+
+        car.owner=null;
+        car.isAvaliable=false;
+        await car.save()
+        res.json({success:true, message:"Availabilty Toggled"})
+    }catch (error){
+        console.log(error.message);
+        res.json({success:false, message:error.message})
+    }
+}
+
+//API to get Dashboard Data
+export const getDashboardData= async (req,res)=>{
+    try{
+        const {_id,role} =req.user;
+        if(role !== 'owner'){
+            return res.json({success: false, message:"Unauthorized"});
+        }
+        const cars =await Car.find({owner:_id})
+    } catch (error){
+        console.log(error.message);
+        res.json({success:false, message:error.message})
+    }
+
+}
--- server/routes/ownerRoutes.js
+++ server/routes/ownerRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
 
-import { addCar, changeRoleToOwner } from "../controllers/ownerController.js";
+import { addCar, changeRoleToOwner, deleteCar, getOwnerCars, toggleCarAvailability } from "../controllers/ownerController.js";
 import { protect } from "../middleware/auth.js";
 import upload from "../middleware/multer.js";
 const ownerRouter=express.Router();
 ownerRouter.post("/change-role", protect, changeRoleToOwner)
 ownerRouter.post("/add-car", upload.single("image"), protect, addCar)
+ownerRouter.post("/cars", protect,getOwnerCars)
+ownerRouter.post("/toggle-car", protect, toggleCarAvailability)
+ownerRouter.post("/delete-car", protect, deleteCar)
 export default ownerRouter;
